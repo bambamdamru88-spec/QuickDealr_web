7f2a478e6977be6dcb858bc681b52cef86b188c4
bambamdamru88-spec <bambamdamru88@gmail.com>
2026-04-28 19:19:12 +0530

    fix werkzeug production error

--- run_admin.py
+++ run_admin.py
@@ -3,4 +3,4 @@ from admin_app import admin_app as app, socketio
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
--- run_user.py
+++ run_user.py
@@ -3,4 +3,4 @@ from user_app import user_app as app, socketio
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
